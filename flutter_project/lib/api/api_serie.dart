4ba49416d2bbf900c5e88823f1c48fda117ff3c7
Carla <100995659+carlaboscolo@users.noreply.github.com>
2022-11-07 15:00:26 +0100

    api key

--- flutter_project/lib/api/api_serie.dart
+++ flutter_project/lib/api/api_serie.dart
@@ -4,12 +4,12 @@ import 'package:http/http.dart' as http;
 //models
 import 'package:flutter_project/models/serie.dart';
 
+const API_KEY = "a7c4848fcfb89f8bef0757f282d0a463";
+
 class ApiSerie {
   Future<List<Serie>> fetchSerie(int page) async {
-    const apiKey = "a7c4848fcfb89f8bef0757f282d0a463";
-
     final response = await http.get(Uri.parse(
-        'https://api.themoviedb.org/3/tv/popular?api_key=$apiKey&language=it&page=$page'));
+        'https://api.themoviedb.org/3/tv/popular?api_key=$API_KEY&language=it&page=$page'));
 
     if (response.statusCode == 200) {
       final result = jsonDecode(response.body);
